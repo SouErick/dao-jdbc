50fad90d94db449fe083e0ea510eabef71df109b
SouErick <ericksousasaraiva@gmail.com>
2024-05-08 11:46:55 -0300

    Inserindo a classe Vendedor

--- src/com/application/Programa.java
+++ src/com/application/Programa.java
@@ -1,12 +1,17 @@
 package com.application;
 
+import java.util.Date;
+
 import com.entities.Departamento;
+import com.entities.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		Departamento obj = new Departamento(1, "Erick");
+		Vendedor vendedor = new Vendedor(1, "Erick", "email@email.com", new Date(), 2000.0, obj);
 		System.out.println(obj);
+		System.out.println(vendedor);
 	}
 
 }

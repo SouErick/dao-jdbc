6faef8f7b2b57405a5409ecb20c186c559b720dd
SouErick <ericksousasaraiva@gmail.com>
2024-05-08 11:36:52 -0300

    Inserindo a classe Departamento e Programa

---
+++ src/com/application/Programa.java
@@ -0,0 +1,12 @@
+package com.application;
+
+import com.entities.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Departamento obj = new Departamento(1, "Erick");
+		System.out.println(obj);
+	}
+
+}
